fb6cf1186c39b10d98a08e796efbaf38ecfa3b8f
Antonio-pf <antoniopfelipe03@gmail.com>
2024-05-29 00:58:15 -0300

    installing larastan, setting up and fix inicial errors

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\User;
+use Illuminate\Contracts\Validation\Rule as ContractRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -11,7 +12,7 @@ class ProfileUpdateRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<string, ContractRule|array<string|ContractRule>|string>
      */
     public function rules(): array
     {
