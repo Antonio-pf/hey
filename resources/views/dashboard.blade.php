45f23796a9f3b9be3b7b15a181f62f9d05591e92
Antonio-pf <antoniopfelipe03@gmail.com>
2024-05-30 17:23:51 -0300

    create the frontend of our form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,45 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+
+            <form action="{{ route('question.store') }}" method="post" class="max-w">
+                @csrf
+                <div class="mb-4">
+                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Question
+                    </label>
+                    <textarea id="question" rows="4" name="question"
+                              class="block p-2.5 w-full
+                              text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300
+                              focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
+                              dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              placeholder="Ask me anything...">{{ old('question') }}</textarea>
+
+
+                    @error('question')
+                    <span class="text-purple-400">
+                            {{ $message }}
+                        </span>
+
+                    @enderror
                 </div>
-            </div>
+
+                <div class="flex gap-2 justify-end">
+                    <button
+                        type="submit"
+                        class=
+                            "focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium
+                             rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                        Save
+                    </button>
+
+                    <button type="reset" class="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400
+                dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        Cancel
+                    </button>
+                </div>
+
+            </form>
         </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/question/store', [\App\Http\Controllers\QuestionController::class, 'store'])->name('questions.store');
+Route::post('/question/store', [\App\Http\Controllers\QuestionController::class, 'store'])->name('question.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
