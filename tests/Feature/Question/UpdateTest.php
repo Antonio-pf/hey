b7320cce765a33eeec8f19933b98304b07417365
Antonio-pf <antoniopfelipe03@gmail.com>
2024-06-17 19:52:28 -0300

    test: update own question

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -4,7 +4,7 @@ use App\Models\Question;
 use App\Models\User;
 use function Pest\Laravel\{actingAs, put, assertDatabaseHas};
 
-it("should create as draft all the time", function () {
+it("should be able to update a question", function () {
     // Arange :: preparar
     $user = User::factory()->create();
     $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
@@ -23,4 +23,29 @@ it("should create as draft all the time", function () {
     expect($question)
         ->question->toBe('Updated question?');
 
+});
+
+
+it('should make sure that only question with status draft can update it', function () {
+    $user = User::factory()->create();
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $draftQuestion = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    actingAs($user);
+
+    // garatir que entra na rota passando a pergunta
+    put(route('question.update', $questionNotDraft))->assertForbidden();
+    put(route('question.update', $draftQuestion), ['question' => 'New question?'])->assertRedirect();
+});
+
+
+it('should make sure that only question own', function () {
+    $user = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true, 'created_by' => $user->id]);
+
+    actingAs($wrongUser);
+    put(route('question.update', $question))->assertForbidden();
+
+    actingAs($user);
+    put(route('question.update', $question), ['question' => 'New question?'])->assertRedirect();
 });
